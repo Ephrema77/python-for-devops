6849b0efa15e12df37d56fead620b14d80a97fb6
Ephrema77 <postfilmart@gmail.com>
2024-04-21 16:18:50 +0000

    File operation change commit

--- Day-12/update_server.py
+++ Day-12/update_server.py
@@ -15,11 +15,11 @@ def update_server_config(file_path, key, value):
                 file.write(line)
 
 # Path to the server configuration file
-server_config_file = 'server.conf'
+#server_config_file = 'server.conf'
 
 # Key and new value for updating the server configuration
-key_to_update = 'MAX_CONNECTIONS'
-new_value = '600'  # New maximum connections allowed
+#key_to_update = 'MAX_CONNECTIONS'
+#new_value = '600'  # New maximum connections allowed
 
 # Update the server configuration file
-update_server_config(server_config_file, key_to_update, new_value)
+update_server_config("server.conf", "MAX_CONNECTIONS", "1200")
